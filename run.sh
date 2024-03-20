3f823beb2a0da2e5bdba88f5047e663c3dcdb778
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2024-03-20 23:43:04 +0100

    remove scripts

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew -q spotlessApply microBundle -x test
-
-java -jar build/libs/regulus-microbundle.jar \
-  --nohazelcast \
-  --autobindhttp \
-  --contextroot '/' \
-  --nocluster
