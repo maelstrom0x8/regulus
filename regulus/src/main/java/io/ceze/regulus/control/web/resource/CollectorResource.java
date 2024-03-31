1312835ac8bbab2cf87a46034c8ebdf73050e880
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2024-03-31 22:50:02 +0100

    endpoint for fetching agent cluster route

--- regulus/src/main/java/io/ceze/regulus/control/web/resource/CollectorResource.java
+++ regulus/src/main/java/io/ceze/regulus/control/web/resource/CollectorResource.java
@@ -15,8 +15,6 @@
  */
 package io.ceze.regulus.control.web.resource;
 
-import io.ceze.regulus.control.service.CollectionService;
-import jakarta.inject.Inject;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -25,11 +23,9 @@ import jakarta.ws.rs.QueryParam;
 @Path("v1/collectors")
 public class CollectorResource {
 
-    @Inject
-    private CollectionService collectionService;
-
     @Path("/agents/{agent_id}")
-    public JsonObject getClusterRoute(@PathParam("agent_id") Long agentId, @QueryParam("cluster") String cluster) {
-        return collectionService.getRouteForCluster(agentId, cluster);
+    public JsonObject getClusterRoute(
+            @PathParam("agent_id") Long agentId, @QueryParam("cluster") String cluster) {
+        return null;
     }
 }
